31366596dc07e2fafb690d51e09d14fa1b07d0c1
mik2k2 <44849223+mik2k2@users.noreply.github.com>
2020-09-17 12:54:50 +0200

    remove PKCS#1 V2.x features + add very basic pre-check before miller-rabin

--- openpgp/rsa.py
+++ openpgp/rsa.py
@@ -2,13 +2,14 @@
 import math
 import secrets
 import itertools
-import operator
 
 
 def find_prime(bitsize, secbits=256):
     rounds = secbits // 2
     while True:
         pot_prime = secrets.randbits(bitsize)
+        if not pot_prime & 1:
+            continue
         if miller_rabin(pot_prime, rounds):
             return pot_prime
 
@@ -64,54 +65,6 @@ def crypt_bytes(key, text):
             .to_bytes(math.ceil(key[1].bit_length() / 8), 'big'))
 
 
-def xor_bytes(a_bytes, b_bytes):
-    return bytes(a ^ b for a, b in zip(a_bytes, b_bytes))
-
-
-def rsaes_oaep_encrypt(key, message, label=b'', *, hash_func):
-    mod_length = math.ceil(key[1].bit_length() / 8)
-    encoded = eme_oaep_encode(message, label, hash_func, mod_length)
-    return crypt_bytes(key, encoded)
-
-
-def rsaes_oaep_decrypt(key, message, label=b'', *, hash_func):
-    mod_length = math.ceil(math.log2(key[1]) / 8)
-    decrypted = crypt_bytes(key, message)
-    return eme_oaep_decode(decrypted, label, hash_func, mod_length)
-
-
-def eme_oaep_encode(message, label, hash_func, mod_length):
-    label_hash = hash_func(label)
-    len_hash = len(label_hash)
-    data_block = b''.join((
-        label_hash,
-        bytes(mod_length - len(message) - 2*len_hash - 2),
-        b'\x01',
-        message
-    ))
-    seed = secrets.token_bytes(len_hash)
-    db_masked = xor_bytes(data_block, mask_gen(hash_func, seed, mod_length - len_hash - 1))
-    seed_masked = xor_bytes(seed, mask_gen(hash_func, db_masked, len_hash))
-    return b''.join((b'\x00', seed_masked, db_masked))
-
-
-def eme_oaep_decode(message, label, hash_func, mod_length):
-    label_hash = hash_func(label)
-    len_hash = len(label_hash)
-    message = message[1:]
-    seed_masked = message[:len_hash]
-    db_masked = message[len_hash:]
-    seed = xor_bytes(seed_masked, mask_gen(hash_func, db_masked, len_hash))
-    data_block = xor_bytes(db_masked, mask_gen(hash_func, seed, mod_length - len_hash - 1))
-    return bytes(itertools.dropwhile(operator.not_, data_block[len_hash:]))[1:]
-
-
-def mask_gen(hash_func, seed, mask_len):
-    return b''.join(hash_func(seed + i.to_bytes(4, 'big'))
-                    for i in range(math.ceil(mask_len / len(hash_func(b''))))
-                    )[:mask_len]
-
-
 def rsaes_pkcs1_v1_5_encrypt(key, message):
     mod_length = math.ceil(math.log2(key[1]) / 8)
     encoded = eme_pkcs1_v1_5_encode(message, mod_length)
@@ -131,18 +84,6 @@ def eme_pkcs1_v1_5_encode(message, mod_length):
 
 def eme_pkcs1_v1_5_decode(message):
     return bytes(itertools.dropwhile(int, message[2:]))[1:]
-
-
-def rsassa_pss_sign(message, key):
-    mod_length = math.ceil(key[1].bit_length() / 8)
-    encoded = emsa_pss_encode(message, mod_length)
-    return crypt_bytes(key, encoded)
-
-
-def rsassa_pss_verify(signature, message, key):
-    mod_length = math.ceil(key[1].bit_length() / 8)
-    decrypted = crypt_bytes(key, signature)
-    return emsa_pss_verify(decrypted, message, key)
 
 
 def emsa_pkcs1_v1_5(message_hash, out_length):
